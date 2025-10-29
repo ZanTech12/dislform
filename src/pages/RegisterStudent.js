37197ea6fb15f7e66806818e7d830d43933166e3
Zantech12 <dontechiel@gmail.com>
2025-10-29 20:08:37 +0100

    ss

--- src/pages/RegisterStudent.js
+++ src/pages/RegisterStudent.js
@@ -132,7 +132,7 @@ function RegisterStudent() {
                         <h2 style={{ color: "#800000" }}>Important Notice</h2>
                         <p>
                             Kindly note that the migration of database of the <strong>Student Management System Database</strong>
-                            database will be taking place tonight by 10:00pm- 3:00am. During this hours, you may experience difficulty in logging in. <br />
+                            database will be taking place tonight by 10:00pm- 3:00am. During these hours, you may experience difficulty in logging in. <br />
                             Thanks in anticipation.</p>
                         <button style={buttonStyle} onClick={handleClosePopup}>
                             OK, I Understand
